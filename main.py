3d5dc743b50ef21e5a4f55254ec8683037af60b2
dongwoooo <ldw9451@gmail.com>
2014-07-28 21:45:04 +0900

    added webtoon crawling

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+#-*- coding:utf-8 -*-
 from flask import Flask, render_template
+from urllib import urlopen
+from bs4 import BeautifulSoup
+import os
+import codecs
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -20,6 +25,17 @@ def idea():
 def photography():
     """Return a friendly HTTP greeting."""
     return render_template('photo.html')
+
+@app.route('/webtoon')
+def webtoon():
+	data = urlopen('http://comics.nate.com/webtoon/detail.php?btno=64923')
+	soup = BeautifulSoup(data)
+	list = []
+	find_div = soup.find('div', {"class" : "toonView"})
+	for img in find_div.findAll('img'):
+		list.append(img['src'])
+	print list
+	return render_template('webtoon.html', list=list)
 
 @app.errorhandler(404)
 def page_not_found(e):
